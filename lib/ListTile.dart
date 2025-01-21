b76b94b85c2bb35dde6df31cd2b1300fedd7ba89
Iliyas Robin <iliyasahmedr@gmail.com>
2025-01-21 19:43:03 +0600

    updating

---
+++ lib/ListTile.dart
@@ -0,0 +1,189 @@
+// Column(
+//   mainAxisAlignment: MainAxisAlignment.start,
+//   crossAxisAlignment: CrossAxisAlignment.start,
+//   children: [
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//     Padding(
+//       padding: const EdgeInsets.all(20),
+//       child: ListTile(
+//         title: Text("Iliyas Ahmed"),
+//         subtitle: Text("BSc. in CSE"),
+//         leading: Icon(Icons.person),
+//         trailing: Icon(Icons.send),
+//       ),
+//     ),
+//
+//
+//
+//   ],
+// ),
