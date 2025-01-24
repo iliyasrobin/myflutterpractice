9dd71363781a59a3280ff4a3a3c530bdaf4d2c99
Iliyas Robin <iliyasahmedr@gmail.com>
2025-01-24 19:26:03 +0600

    updating

--- lib/Column.dart
+++ lib/Column.dart
@@ -1,53 +1,53 @@
-// import 'package:flutter/material.dart';
-//
-// void main(){
-//   runApp(MyApp());
-// }
-//
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: HomeActivity(),
-//     );
-//   }
-// }
-//
-// class HomeActivity extends StatelessWidget {
-//   const HomeActivity({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("My App", style: TextStyle(
-//             fontSize: 40,
-//             backgroundColor: Colors.blue,
-//             color: Colors.white
-//         ),
-//         ),
-//         foregroundColor: Colors.white,
-//         backgroundColor: Colors.blue,
-//         centerTitle: true,
-//
-//       ),
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Text("Hello World"),
-//           Text("Hello World"),
-//           Text("Hello World"),
-//           Text("Hello World"),
-//           Text("Hello World"),
-//
-//         ],
-//       ),
-//     );
-//   }
-// }
-//
-//
-//
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeActivity(),
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My App", style: TextStyle(
+            fontSize: 40,
+            backgroundColor: Colors.blue,
+            color: Colors.white
+        ),
+        ),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Hello World"),
+          Text("Hello World"),
+          Text("Hello World"),
+          Text("Hello World"),
+          Text("Hello World"),
+
+        ],
+      ),
+    );
+  }
+}
+
+
+
