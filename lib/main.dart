64f0344eecfd7d6bc1f34f2fa48147bfc069315b
Iliyas Robin <iliyasahmedr@gmail.com>
2025-01-24 19:13:35 +0600

    updating

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,117 +11,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      home: HomeActivity(),
     );
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController _nameController = TextEditingController();
-    TextEditingController _numberController = TextEditingController();
-    TextEditingController _emailController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: Text("My App"),
-        backgroundColor: Colors.blue,
+        title: Text("My App", style: TextStyle(
+          fontSize: 40,
+          backgroundColor: Colors.blue,
+          color: Colors.white
+        ),
+        ),
         foregroundColor: Colors.white,
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: TextField(
-              controller: _nameController,
-              decoration: InputDecoration(
-                hintText: "Enter your name",
-                helperText: "Please enter your name",
-                prefix: Text("Name: "),
-// suffix: Text("@gmail.com"),
-                label: Text("Enter your name:"),
-// filled: false,
-// fillColor: Colors.green,
-              ),
-            ),
-          ),
+          Text("Hello World"),
+          Text("Hello World"),
+          Text("Hello World"),
+          Text("Hello World"),
+          Text("Hello World"),
 
-//using divider
-          Divider(),
-//using divider
-
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: TextField(
-              controller: _numberController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                hintText: "Enter your Number",
-                helperText: "Please enter your number",
-                prefix: Text("+880"),
-                label: Text("Enter your number:"),
-              ),
-            ),
-          ),
-
-
-//using divider
-          Divider(),
-//using divider
-
-
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                hintText: "Enter your email",
-                helperText: "Please enter your name",
-                label: Text("Enter your email:"),
-                suffix: Text("@gmail.com"),
-              ),
-            ),
-
-          ),
-
-
-//using divider
-          Divider(),
-//using divider
-
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: ElevatedButton(onPressed: (){
-                print("Submit button pressed");
-              }, child: Text("Submit"),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.white,
-
-                  )),
-            ),
-          )
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print("floating button pressed");
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.red,
-        foregroundColor: Colors.white,
       ),
     );
   }
 }
-
-
 
 
 
