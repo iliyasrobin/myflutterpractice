22a6743f198e44ce2d3b071404bb15f066c8f428
Iliyas Robin <iliyasahmedr@gmail.com>
2025-01-25 19:59:14 +0600

    updating

---
+++ lib/Center.dart
@@ -0,0 +1,45 @@
+// import 'package:flutter/material.dart';
+//
+// void main(){
+//   runApp(MyApp());
+// }
+//
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: HomeActivity(),
+//     );
+//   }
+// }
+//
+// class HomeActivity extends StatelessWidget {
+//   const HomeActivity({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("My App", style: TextStyle(
+//             fontSize: 40,
+//             backgroundColor: Colors.blue,
+//             color: Colors.white
+//         ),
+//         ),
+//         foregroundColor: Colors.white,
+//         backgroundColor: Colors.blue,
+//         centerTitle: true,
+//
+//       ),
+//       body: Center(
+//         child: Text("Hello World"),
+//       ),
+//     );
+//   }
+// }
+//
+//
+//
