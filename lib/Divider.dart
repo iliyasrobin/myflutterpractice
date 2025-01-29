1b091064dc8d781ce00fbdcefbdd14bd283426bb
Iliyas Robin <iliyasahmedr@gmail.com>
2025-01-29 17:55:13 +0600

    updating

---
+++ lib/Divider.dart
@@ -0,0 +1,50 @@
+// import 'package:flutter/material.dart';
+//
+//
+// void main(){
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Home(),
+//     );
+//   }
+// }
+//
+// class Home extends StatelessWidget {
+//   const Home({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("MyApp"),
+//         backgroundColor: Colors.blue,
+//         foregroundColor: Colors.white,
+//         centerTitle: true,
+//       ),
+//       body: Center(
+//         child: Column(
+//           children: [
+//             Text("ABC"),
+//             Divider(
+//               color: Colors.red,
+//               thickness: 20,
+//             ),
+//             Text("DEF"),
+//           ],
+//         ),
+//       ),
+//
+//     );
+//   }
+// }
+//
+//
+//
+//
--- lib/ListTile.dart
+++ lib/ListTile.dart
@@ -1,7 +1,24 @@
 // Column(
 //   mainAxisAlignment: MainAxisAlignment.start,
-//   crossAxisAlignment: CrossAxisAlignment.start,
 //   children: [
+//     //Extended
+// // ListTile(
+// // title: Text("ILIYAS AHMED"),
+// // subtitle: Text("BSc. in CSE"),
+// // leading: Icon(Icons.account_circle,size: 40,),
+// // trailing: Icon(Icons.send, size: 40,),
+// // onTap: (){
+// // print("I am List Tile");
+// // },
+// // onLongPress: (){
+// // print("I am ListTile Long Press");
+// // },
+// // ),
+//
+//
+//
+//
+//
 //     Padding(
 //       padding: const EdgeInsets.all(20),
 //       child: ListTile(
