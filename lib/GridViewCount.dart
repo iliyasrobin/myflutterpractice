7969d4f003c6b6f34954b65a0dfdab9bedd46150
Iliyas Robin <iliyasahmedr@gmail.com>
2025-02-01 10:28:46 +0600

    updating

---
+++ lib/GridViewCount.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class GCount extends StatelessWidget {
+  final List<String> categories = [
+    "Fruits",
+    "vegetables",
+    "Meats",
+    "Fishes",
+    "Bakery",
+    "snacks",
+    "drinks",
+    "frozen items"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Wrap"),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Wrap(
+              spacing: 10,
+              runSpacing: 20,
+              children: categories.map((category) {
+                return Chip(
+                  label: Text(category),
+                  backgroundColor: Colors.blueAccent,
+                );
+              }).toList(),
+            ),
+            Expanded(
+              child: GridView.count(
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                crossAxisCount: 3,
+                children: List.generate(
+                    15,
+                    (index) => Chip(
+                          label: Text("Item ${index}"),
+                          backgroundColor: Colors.blue,
+                        )),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mypractice/GridViewCount.dart';
 import 'package:mypractice/mediaQuery.dart';
 import 'package:mypractice/wrapWidget.dart';
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: wrapWidget(),
+      home: GCount(),
     );
   }
 }
